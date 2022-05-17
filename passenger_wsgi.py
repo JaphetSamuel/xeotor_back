1f53971cc5ba5ccb6910818fb0e535c18925dc3a
unknown <sagoua1@gmail.com>
2022-05-17 23:06:15 +0000

    gestion erreur bd

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,8 +2,25 @@ import os
 import sys
 from main import app
 from a2wsgi import ASGIMiddleware
+from pydbantic import Database
+from client.main import tables as client_tables
+from driver.main import tables as driver_tables
 
-print("quelqque chose")
+
+async def init_db():
+    print("initialisaztion de la bd dans init")
+    try:
+        db = await Database.create(
+            "sqlite:///./xeotor.db",
+            tables=client_tables + driver_tables,
+            redis_url="redis://localhost"
+        )
+    except Exception as e:
+        print("erreur d'initialisationde la bd")
+        print(e)
+        print(e.args)
+
+await init_db()
 
 application = ASGIMiddleware(app)
 
