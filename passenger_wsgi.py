a238546a03a20685e0f5b973c653a64519d7785b
unknown <sagoua1@gmail.com>
2022-05-18 04:27:45 +0000

    make it better

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,29 +2,12 @@ import os
 import sys
 from main import app
 from a2wsgi import ASGIMiddleware
-from pydbantic import Database
-from client.main import tables as client_tables
-from driver.main import tables as driver_tables
-import asyncio
 
-# print("appel de passenger")
-# async def init_bd():
-#     print("initialisaztion de la bd dans init")
-#     try:
-#         db = await Database.create(
-#             "sqlite:///./xeotor.db",
-#             tables=client_tables + driver_tables,
-#             redis_url="redis://localhost"
-#         )
-#     except Exception as e:
-#         print("erreur d'initialisationde la bd")
-#         print(e)
-#         print(e.args)
 
 
 application = ASGIMiddleware(app)
 
-# asyncio.run(init_bd())
+
 
 
 sys.path.insert(0, os.path.dirname(__file__))
